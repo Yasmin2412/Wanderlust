aff1c723000c015cec441bb2d1dc0fe413d55d64
[Yasmin khan] <[yasmin.jpr25@gmail.com]>
2024-07-16 13:05:10 +0530

    Update repository

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
 const { required } = require('joi');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const  passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema= new Schema({
-    email : {
+const userSchema = new Schema({
+    email: {
         type: String,
-        required: true
+        require: true,
     }
-    
 });
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', userSchema);
+
+userSchema.plugin(passportLocalMongoose); 
+
+module.exports = mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,39 @@ const app = express();
 const router=express.Router()
 const User=require("../models/user.js")
 const passport = require('passport');
+const flash=require("connect-flash")
+const bodyParser = require('body-parser');
+const wrapAsync = require("../utils/WrapAsync.js");
+const LocalStrategy = require('passport-local').Strategy;
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/signup",(req,res)=>{
     res.render("./users/signup.ejs")
 })
 router.post("/signup", async(req,res)=>{
-    let { username,email,password} =req.body;
+    try{
+        let { username,email,password} =req.body;
     const newUser= new User({email,username});
     const registeredUser=await User.register(newUser,password)
     console.log(registeredUser);
     req.flash("Done","Welcome to Wanderlust")
     res.redirect("/listings")
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup")
+    }
 })
+router.get("/login",(req,res)=>{
+    res.render("./users/login.ejs")
+})
+
+router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true
+}), wrapAsync(async (req, res) => {
+    res.send('You Login Successfully');
+}));
+
+app.use('/', router);
 module.exports=router;
